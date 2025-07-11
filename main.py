9cdcae72a0f7e7bd368153b09fa3350e16a4b2df
Manvel <a@b.ru>
2025-07-11 09:34:06 +0300

    Class Bank Account

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
-# OOП ДЗ
+# OOП Class
 """
-Задача: смоделировать зоопарк с разными животными.
+Задача: создать классы для моделирования клиентов банка и их счетов.
 Условия:
-	Базовый класс Animal с методом make_sound().
-	Классы-наследники: Dog, Cat, Elephant с переопределением звуков.
-	Класс Zoo хранит список животных и метод make_all_sounds().
+	Класс BankAccount с атрибутами: _owner_name, balance.
+	Методы: deposit(amount), withdraw(amount), get_balance().
 
 """
 
+
+class BankAccount:
+    def __init__(self, owner, balance=0):
+        self._owner = owner
+        self._balance = balance
+
+    def get_balance(self):
+        return self._balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self._balance += amount
+            print(f'Депозит пополнен на сумму {amount}.')
+        else:
+            print(f'Нельзя вносить отрицательную сумму на депозит.')
+
+    def withdraw(self, amount):
+        if 0 < amount <= self._balance:
+            self._balance -= amount
+            print(f'С депозита снята сумма {amount}.')
+        else:
+            print(f'Не хватает средств. Овердрафт не доступен.')
+
+
+client1 = BankAccount('John')
+client1.deposit(500)
+client1.withdraw(600)
+print('Остаток:', client1.get_balance())
 
 # OOП (inheritance)
 # класс, от которого наследуем: базовый, родительский, суперкласс
