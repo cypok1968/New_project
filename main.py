108e7c122d91ff67b103a8f9bf5258874374db28
Manvel <a@b.ru>
2025-07-11 12:13:32 +0300

    NET Intro and CLI

--- main.py
+++ main.py
@@ -13,3 +13,17 @@
 # ASCII %20, %2C
 # URL - Uniform Resource Locator
 # http(s)://домен.зона/page1/?param1=value1&param2=value2
+# ../font/
+import sys
+
+print('Я', sys.argv[0], 'и мой аргумент', sys.argv[1])
+
+if len(sys.argv) >= 2:
+    match sys.argv[1]:
+        case 'p':
+            print('Привет')
+        case 'g':
+            print('Пока')
+        case _:
+            print('Не понял')
+
