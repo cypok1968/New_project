3abd9e1566a5372785bc6a9c057f3f7d1e26b876
Manvel <a@b.ru>
2025-07-14 11:19:01 +0300

    Java Script Intro

---
+++ js/sample.js
@@ -0,0 +1,37 @@
+// Скрипт sample.js
+/**************************
+Здесь некоторые элементы языка
+подробнее здесь: https://learn.javascript.ru
+**************************/
+
+// Функция
+function sayHello(name) {
+    document.writeln("Вас зовут " + name);
+}
+
+// Переменная (var или let)
+/* let name = prompt("Ваше имя: ");
+sayHello(name); // вызов функции */
+
+// Массивы
+let colors = ["Красный", "Синий", "Голубой"];
+
+/*document.writeln("<h1>Цвета:</h1><ol>");
+// Цикл for
+for(let i=0; i<colors.length; i++) {
+    document.writeln("<li>" + colors[i] + "</li>");
+}
+document.write("</ol>")*/
+
+function changeColor() {
+    // document.getElementById('alive').style.color = 'red';
+    const txt = document.getElementById('alive');
+    if(txt.style.display === 'none') {
+    txt.style.display = block;
+    } else {
+       txt.style.display = 'none';
+    }
+}
+
+//Подключаюсь к элементу в DOM
+const txt = document.getElementById('alive').onclick = changeColor;
