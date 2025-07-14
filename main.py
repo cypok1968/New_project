9a5e9609d0570a6456ed376a808ad495647db898
Manvel <a@b.ru>
2025-07-14 15:40:26 +0300

    JSON Write

--- main.py
+++ main.py
@@ -4,18 +4,33 @@
 # loads() - читает строковое представление
 import json
 
-with open('dogs.json', 'rt') as d:
-    # data = json.load(d) # Напрямую из файла
-    temp = d.read()  # читаем файл как строку
-    data = json.loads(temp)  # строковое представление JSON
+d = {
+    'ананас': 300,
+    'банан': 400,
+    'яблоко': 120,
+    'груша': 280,
+}
 
-for i in range(len(data)):
-    print(f'Питомец №{i + 1}:')
-    for k, v in data[i].items():
-        if type(v) == list:
-            print(f'\t{k}: {', '.join(v)}')
-        else:
-            print(f'\t{k}: {v}')
+# запись напрямую в файл
+# with open('fruits.json', 'w', encoding='utf-8') as f:
+#     json.dump(d, f, indent=4)
+
+# вывод в виде строки
+data = json.dumps(d, indent=4)
+print(data)
+
+# with open('dogs.json', 'rt') as d:
+#     # data = json.load(d) # Напрямую из файла
+#     temp = d.read()  # читаем файл как строку
+#     data = json.loads(temp)  # строковое представление JSON
+#
+# for i in range(len(data)):
+#     print(f'Питомец №{i + 1}:')
+#     for k, v in data[i].items():
+#         if type(v) == list:
+#             print(f'\t{k}: {', '.join(v)}')
+#         else:
+#             print(f'\t{k}: {v}')
 
 # for k, v in data.items():
 #     if type(v) == list:
