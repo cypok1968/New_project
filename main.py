3ce6cbade5396fee9e851e294ee9ef772178c490
Manvel <a@b.ru>
2025-07-14 15:29:54 +0300

    JSON Read

--- main.py
+++ main.py
@@ -9,11 +9,19 @@ with open('dogs.json', 'rt') as d:
     temp = d.read()  # читаем файл как строку
     data = json.loads(temp)  # строковое представление JSON
 
-for k, v in data.items():
-    if type(v) == list:
-        print(f'{k}: {', '.join(v)}')
-    else:
-        print(f'{k}: {v}')
+for i in range(len(data)):
+    print(f'Питомец №{i + 1}:')
+    for k, v in data[i].items():
+        if type(v) == list:
+            print(f'\t{k}: {', '.join(v)}')
+        else:
+            print(f'\t{k}: {v}')
+
+# for k, v in data.items():
+#     if type(v) == list:
+#         print(f'{k}: {', '.join(v)}')
+#     else:
+#         print(f'{k}: {v}')
 
 # Zip
 # from zipfile import ZipFile
