64c097a1ee45102f8136a4dd503a41a7d8799735
Manvel <a@b.ru>
2025-07-14 14:40:43 +0300

    Zip Files

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-# CSV-файлы
-import csv
+# Zip
+from zipfile import ZipFile
+import os
+
+# csv_files = [f for f in os.listdir() if f.endswith('.csv')]
+# # print(csv_files)
+# with ZipFile('archive.zip', 'w') as myzip:
+#     for file in csv_files:
+#         myzip.write(file)
+#         os.remove(file)
+files_to_extract = ['people.csv', 'file.csv']
+
+# Распаковать
+# with ZipFile('archive.zip', 'r') as zip_obj:
+#     zip_obj.extractall(members=files_to_extract)
+
+# Получить список
+with ZipFile('archive.zip', 'r') as zip_obj:
+    print(zip_obj.namelist())
+
+
+# CSV-файлы (strptime)
+# import csv
 
 # with open('people.csv', 'r', encoding='utf-8') as f:
 #     dict_reader = csv.DictReader(f)
@@ -18,10 +39,10 @@ import csv
 #     writer.writerow(data)
 
 # Режимы квотирования
-data = ['name', 25, 'town']
-with open('sample.csv', 'w', newline='', encoding='utf-8') as f:
-    writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-    writer.writerow(data)
+# data = ['name', 25, 'town']
+# with open('sample.csv', 'w', newline='', encoding='utf-8') as f:
+#     writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+#     writer.writerow(data)
 
 # data = [
 #     ['name', 'age', 'city'],
