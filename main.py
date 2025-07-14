bef8d98d3470a767ce8deaef459c1ceaacf57685
Manvel <a@b.ru>
2025-07-14 14:26:09 +0300

    CSV Dict adn Quoting

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 # CSV-файлы
 import csv
 
-data = [
-    ['name', 'age', 'city'],
-    ['Борис', '25', 'Воронеж'],
-    ['Владимир', '28', 'Тверь'],
-    ['Глеб', '35', 'Москва']
-]
+# with open('people.csv', 'r', encoding='utf-8') as f:
+#     dict_reader = csv.DictReader(f)
+#     for row in dict_reader:
+#         print(f'{row['name']} живёт в городе {row['city']}')
 
-with open('people.csv', 'r', encoding='utf-8') as f:
-    reader = csv.reader(f, delimiter=',', quotechar='"')
-    for row in reader:
-        print(row)
+# field_names = ['name','age','city']
+# data = {
+#     'name' : 'Борис',
+#     'age': 27,
+#     'city': 'Москва'
+# }
+#
+# with open('file.csv', 'w', newline='', encoding='utf-8') as f:
+#     writer = csv.DictWriter(f, fieldnames=field_names)
+#     writer.writerow(data)
 
-with open('employee.csv', 'w', newline='', encoding='utf-8') as f:
-    writer = csv.writer(f)
-    writer.writerows(data)
+# Режимы квотирования
+data = ['name', 25, 'town']
+with open('sample.csv', 'w', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+    writer.writerow(data)
+
+# data = [
+#     ['name', 'age', 'city'],
+#     ['Борис', '25', 'Воронеж'],
+#     ['Владимир', '28', 'Тверь'],
+#     ['Глеб', '35', 'Москва']
+# ]
+#
+# with open('people.csv', 'r', encoding='utf-8') as f:
+#     reader = csv.reader(f, delimiter=',', quotechar='"')
+#     for row in reader:
+#         print(row)
+#
+# with open('employee.csv', 'w', newline='', encoding='utf-8') as f:
+#     writer = csv.writer(f)
+#     writer.writerows(data)
