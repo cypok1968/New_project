ed484ed92d7a6e8affa06bb776bf71f137fbbd84
Manvel <a@b.ru>
2025-07-15 15:53:30 +0300

    Decorator NonLocal

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 # Декораторы
-def upper_case_print(old_func):
-    def new_func(*args, **kwargs):
-        case = kwargs.pop('case', None)
-        if case == 'U':
-            args = [str(arg).upper() for arg in args]
-        elif case == 'L':
-            args = [str(arg).lower() for arg in args]
-        return old_func(*args, **kwargs)
-    return new_func
+def outer():
+    x = 5
 
-new_print = upper_case_print(print)
-new_print('Привет, Пока')
-new_print('Привет, Пока', case='U')
-new_print('Привет, Пока', case='L')
+    def inner():
+        nonlocal x
+        print('Nonlocal x=', x)
+        x = 10
+
+    inner()
+    print('New x=', x)
+
+
+outer()
+
+# def upper_case_print(old_func):
+#     def new_func(*args, **kwargs):
+#         case = kwargs.pop('case', None)
+#         if case == 'U':
+#             args = [str(arg).upper() for arg in args]
+#         elif case == 'L':
+#             args = [str(arg).lower() for arg in args]
+#         return old_func(*args, **kwargs)
+#     return new_func
+#
+# new_print = upper_case_print(print)
+# new_print('Привет, Пока')
+# new_print('Привет, Пока', case='U')
+# new_print('Привет, Пока', case='L')
 
 # def answer(question):
 #     return 'думайте сами'
