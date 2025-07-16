369130e3159480b90ea1f80b5bb853104267a019
Manvel <a@b.ru>
2025-07-16 11:48:11 +0300

    Sample Page HTML

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Введение во Flask
 # MVC-(Model View Controller)
-from flask import Flask
+from fileinput import filename
+
+from flask import Flask, url_for
 
 app = Flask(__name__)
 debug = False
@@ -24,6 +26,26 @@ def cd():
     lst = [str(x) for x in reversed(range(10))]
     lst.append('Полетели!!!')
     return '<br>'.join(lst)
+
+
+@app.route('/image')
+def show_image():
+    return f'<img src="{url_for('static', filename='images/python.jpg')}">'
+
+
+@app.route('/sample-page')
+def sample_page():
+    return f"""<!DOCTYPE html>
+            <html lang="ru">
+            <head>
+                <meta charset="UTF-8">
+                <title>Картинка Питона</title>
+            </head>
+            <body>
+                <img src="{url_for('static', filename='images/python.jpg')}" alt="Python">
+            </body>
+            </html>
+    """
 
 
 if __name__ == '__main__':
