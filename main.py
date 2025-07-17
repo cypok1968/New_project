93193181dfa3ff03a77c4f13b8a076498be224d7
Manvel <a@b.ru>
2025-07-17 10:44:20 +0300

    File Upload

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 # Введение во Flask
 # MVC-(Model View Controller)
+# GET - запрашивает данные (read)
+# POST - отправляет данные на сервер (submit)
+# PUT - заменяет всё на сервере из контекста запроса ("заменить")
+# DELETE - удаляет указанные данные ("удалить")
+# PATCH - частичное изменение данных
+import os.path
+
 from flask import Flask, url_for, request
+from werkzeug.utils import secure_filename
 import sqlite3
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+ALLOWED_EXTENSIONS = ['txt', 'pdf', 'zip', 'jpg', 'png']
 debug = False
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
@@ -105,6 +120,28 @@ def form_test():
     elif request.method == 'POST':
         print(request.form)
         return 'Форма успешно отправлена'
+
+
+@app.route('/upload', methods=['POST', 'GET'])
+def file_upload():
+    if request.method == 'GET':
+        with open('upload.html', 'r', encoding='utf-8') as html:
+            return html.read()
+    elif request.method == 'POST':
+        # print(request.files)
+        if 'file' not in request.files:
+            return 'Файл не был выбран!!!'
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return 'Файл не был выбран!!!'
+
+        if file and allowed_file(file.filename):
+            new_name = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], new_name))
+            return f'Файл {new_name} успешно загружен!'
+    return "Ошибка загрузки"
 
 
 if __name__ == '__main__':
