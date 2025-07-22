d852d840afbeef6f4354a042ccc9e4176a76fc54
Anton <cypku@mail.ru>
2025-07-22 13:07:51 +0300

    Mail Send

--- mail_test.py
+++ mail_test.py
@@ -1,6 +1,6 @@
 # mail_test.py
 # pip install python-dotenv
+# from dotenv import load_dotenv
 from send_mail import send_mail
-from dotenv import load_dotenv
 
 send_mail('acypok@mail.ru', 'Вам письмо', 'Текст письма')
